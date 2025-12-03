22218d1cfce3866b64afff7a100b0cb7290e4242
Nhlalo <nhlalonkosi@gmail.com>
2025-12-03 21:37:32 +0200

    Implement SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import "./css/index.css";
+import router from "./route.jsx";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
